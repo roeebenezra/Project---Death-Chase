06a77ec420fdc977395633c9bf5b4bce2c12bde5
Roee <roibenezra0@gmail.com>
2022-06-18 01:12:03 +0300

    fixed user car move

--- cmake-build-debug/src/cmake_install.cmake
+++ cmake-build-debug/src/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/roeebenezra/Desktop/project/src
+# Install script for directory: /Users/roeebenezra/CLionProjects/project/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
